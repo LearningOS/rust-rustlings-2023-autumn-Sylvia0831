ced1398505ea9c383198789113386b9f2e6c14ae
sylvia <735347849@qq.com>
2023-10-14 06:38:58 -0700

    update

--- exercises/structs/structs3.rs
+++ exercises/structs/structs3.rs
@@ -29,12 +29,16 @@ impl Package {
         }
     }
 
-    fn is_international(&self) -> ??? {
-        // Something goes here...
+    fn is_international(&self) -> bool {
+       if self.sender_country != self.recipient_country{
+            true
+       } else {
+            false
+       }
     }
 
-    fn get_fees(&self, cents_per_gram: i32) -> ??? {
-        // Something goes here...
+    fn get_fees(&self, cents_per_gram: i32) -> i32 {
+        cents_per_gram*self.weight_in_grams
     }
 }
 
